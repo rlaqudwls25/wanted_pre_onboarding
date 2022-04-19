8c1a82792be735b3a2bef02ea7cd326fc295177b
rlaqudwls25 <rlaqudwls157@gmail.com>
2022-04-19 22:30:54 +0900

    전체 컴포넌트 style 잡기 및 Tab 구현 완료

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import Tab from "./Components/Tab";
 import Input from "./Components/Input";
 import Dropdown from "./Components/Dropdown";
 import Slider from "./Components/Slider";
+import styled from "styled-components";
+import { mixin } from "./Styles/mixin";
 
 function App() {
   return (
-    <>
+    <Wrapper>
       <Toggle />
       <Tab />
       <Input />
       <Slider />
       <Dropdown />
-    </>
+    </Wrapper>
   );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  ${mixin.flexSet("space-evenly", "center", "_")}
+  margin-top: 100px;
+`;
--- src/Components/Tab.js
+++ src/Components/Tab.js
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import { mixin } from "../Styles/mixin";
+import { TabMenu } from "../Config/config";
 
 const Tab = () => {
-  return <div>Tab</div>;
+  const [name, setName] = useState("감자");
+
+  const onChangeTab = (name) => {
+    setName(name);
+  };
+
+  return (
+    <TabContainer>
+      {TabMenu.map((item) => {
+        return (
+          <>
+            <TabList onClick={() => onChangeTab(item)}>{item}</TabList>
+          </>
+        );
+      })}
+      <TabBottomSlider name={name} />
+    </TabContainer>
+  );
 };
 
 export default Tab;
+
+const TabContainer = styled.div`
+  ${mixin.flexSet("space-evenly", "center")}
+  width: 400px;
+  height: 50px;
+  position: relative;
+  background-color: #eeeeee;
+`;
+
+const TabList = styled.div`
+  ${mixin.flexSet("center", "center")}
+  width: 33%;
+  cursor: pointer;
+`;
+
+const TabBottomSlider = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 33%;
+  ${mixin.borderSet("1", "solid", "black")}
+  transform: translateX(
+    ${({ name }) => {
+    if (name === "감자") {
+      return "0%";
+    } else if (name === "고구마") {
+      return "100%";
+    } else if (name === "카레라이스") {
+      return "200%";
+    }
+  }}
+  );
+  transition: 1s;
+`;
